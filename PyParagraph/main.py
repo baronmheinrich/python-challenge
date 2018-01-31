abab9c80be4890f421d5fbdd6a25b5bded7c8ad6
acanois <david.richter42@gmail.com>
2018-01-31 14:40:51 -0600

    add full PyParagraph assignment

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,16 +1,17 @@
 f = open('Resources/flying.txt', 'r')
 
-wordcount = 0
-sentence_count = 0
-
 reader = f.read()
 words = reader.split()
 n_words = len(words)
 n_sentences = reader.count('.')
 avg_words = sum(len(word) for word in words) / n_words
 
+sents = reader.split('.')
+
+avg_sents = sum(float(len(sent.split(' '))) for sent in sents) / float(len(sents))
 print('\nParagraph Analysis')
 print('------------------')
 print('Approximate word count: {}'.format(n_words))
 print('Approximate sentence count: {}'.format(n_sentences))
 print('Average Letter Count: {}'.format(avg_words))
+print('Average Sentence Length: {}'.format(avg_sents))
